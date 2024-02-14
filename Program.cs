29c3312b36e3905d5ff0df2ca355b82d4d291bf7
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-02-14 10:28:03 -0300

    feat: create class ChessPosition

--- Program.cs
+++ Program.cs
@@ -3,21 +3,9 @@ using board;
 using chess;
 using xadrez_console;
 
-try 
-{
-    Board b = new(8, 8);
+ChessPosition cp = new('c', 7);
 
-    b.SetPiece(new Tower(b, Color.Black), new Position(0, 0));
-    b.SetPiece(new King(b, Color.Black), new Position(0, 10));
-    b.SetPiece(new King(b, Color.Black), new Position(3, 4));
-    b.SetPiece(new Tower(b, Color.Black), new Position(1, 0));
-
-    Screen.PrintBoard(b);
-}
-catch (Exception e) 
-{
-    System.Console.WriteLine(e.Message);
-}
-
+System.Console.WriteLine(cp);
+System.Console.WriteLine(cp.ToPosition());
 
 Console.ReadLine();
